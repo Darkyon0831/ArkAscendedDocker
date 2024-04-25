1dc907b56d8aeddc29a5aaadf3fcf8b662dadd13
Adrian Rondahl <adrian.rondahl@live.se>
2024-04-25 22:40:49 +0200

    Update Update_ark.sh

--- scripts/Update_ark.sh
+++ scripts/Update_ark.sh
@@ -1,1 +1,1 @@
-$STEAM_CMD_DIR +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir $ARK_FOLDER +app_update 2430930 +quit
+$STEAM_CMD_DIR/steamcmd +login anonymous +@sSteamCmdForcePlatformType windows +force_install_dir $ARK_FOLDER +app_update 2430930 +quit
