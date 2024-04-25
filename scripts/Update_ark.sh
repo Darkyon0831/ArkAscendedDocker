3b93104101eb8063fdb163c2bc74f410ef141f48
Adrian Rondahl <adrian.rondahl@live.se>
2024-04-26 00:23:49 +0200

    Update Update_ark.sh

--- scripts/Update_ark.sh
+++ scripts/Update_ark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $ARK_FOLDER
 $STEAM_CMD_DIR/steamcmd.sh +force_install_dir "$ARK_FOLDER" +login anonymous +@sSteamCmdForcePlatformType windows +app_update 2430930 +quit
-while true
-do
-
-done
+cat /home/dark/dark/Steam/steamapps/appmanifest_2430930.acf
+find "/home/dark/dark/Steam/steamapps/common/Ark Survival Ascended Dedicated Server" -type f -printf "%f\n"
+ls -d "/home/dark/dark/Steam/steamapps/common/Ark Survival Ascended Dedicated Server/*/"
+ls -d "/home/dark/arkserver/*/"
