6e3c8ef994e166a7cb15d8e0ce480ad300a9320e
adrian <adrian.rondahl@live.se>
2024-04-27 13:39:28 +0200

    Update_ark.sh

--- scripts/Update_ark.sh
+++ scripts/Update_ark.sh
@@ -1,14 +1,24 @@
+# MIT License
+
+# Copyright (c) 2024 Adrian Rondahl
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 #!/bin/bash
-echo $ARK_FOLDER
-$STEAM_CMD_DIR/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir /home/dark/arkserver +login anonymous +app_update 2430930 +quit
-echo "\nManifest"
-cat /home/dark/dark/Steam/steamapps/appmanifest_2430930.acf
-
-echo "\nFind"
-find "/home/dark/dark/Steam/steamapps/common/ARK Survival Ascended Dedicated Server" -type f -printf "%f\n"
-
-echo "\nSteam"
-ls "/home/dark/dark/Steam/steamapps/common/ARK Survival Ascended Dedicated Server"
-
-echo "\nArkserver"
-ls "/home/dark/arkserver"
+$STEAM_CMD_DIR/steamcmd.sh +@sSteamCmdForcePlatformType windows +force_install_dir $ARK_FOLDER +login anonymous +app_update 2430930 +quit
